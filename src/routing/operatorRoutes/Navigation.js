496cd12bcf765ea43d54e9fb6bcc4a2a8a1b846d
YanPalyn <Kvinto1986@gmail.com>
2020-02-06 10:29:02 +0300

    fix routing

--- src/routing/operatorRoutes/Navigation.js
+++ src/routing/operatorRoutes/Navigation.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import HomeScreen from "../../components/HomeScreen";
-import ScanTable from "../../components/scaner/responceComponents/ScanResponceContainer";
-import ScannerScreen from './../../screens/ScannerScreen';
+import HomeScreen from "../../../components/operatorPage/HomeScreen";
+import ScanTable from "../../../components/scaner/responceComponents/ScanResponceContainer";
+import ScannerScreen from './../../../screens/ScannerScreen';
 
 const AppNavigator = createStackNavigator({
     Home: {
