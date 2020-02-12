cdff4a71ac118779aa9bd852d8208db582ee7831
Alexey-Kleschov <iron-goga@yandex.ru>
2020-02-12 15:31:41 +0300

    driver authorization

--- actions/authAction.js
+++ actions/authAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import {CURRENT_USER, ERRORS,SERVICE} from './types';
+import {CURRENT_USER, ERRORS, SERVICE} from './types';
 import jwt_decode from 'jwt-decode';
-import {serverConfig} from '../serverConfig';
+import { warehouseConfig, cargoConfig } from '../serverConfig';
 
 export const setCurrentUser = (decoded, token) => {
     return {
@@ -11,15 +11,16 @@ export const setCurrentUser = (decoded, token) => {
     };
 };
 
-export const loginUser = (user, spinner) => dispatch => {
+export const loginUser = (user, spinner, service = '') => dispatch => {
+    const URL = !service ? warehouseConfig : cargoConfig;
     axios
-        .post(`${serverConfig}login`, user)
+        .post(`${URL}login`, user)
         .then(res => {
             const {token} = res.data;
             const decoded = jwt_decode(token);
             dispatch(setCurrentUser(decoded, token));
         })
-        .then(() => {
+        .then(() => { 
             dispatch({
                 type: ERRORS,
                 payload: {},
--- serverConfig.js
+++ serverConfig.js
@@ -1,1 +1,2 @@
-export const serverConfig = 'https://warehouse-online-backend.herokuapp.com/api/';
+export const warehouseConfig = 'https://warehouse-online-backend.herokuapp.com/api/';
+export const cargoConfig = 'https://mega-trucking-api.herokuapp.com/';
