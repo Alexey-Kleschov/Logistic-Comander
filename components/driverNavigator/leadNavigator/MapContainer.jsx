d9ec5b0a1f1b1727dfb549ac424d7a87b47c0353
Maksim Yepifanau <Maksim.Yepifanau@itechart-group.com>
2020-02-20 10:39:59 +0300

    polyline attempt

--- components/driverNavigator/leadNavigator/MapContainer.jsx
+++ components/driverNavigator/leadNavigator/MapContainer.jsx
@@ -3,13 +3,14 @@ import MapNavigator from '../../../common/maps/MapNavigator';
 import { Platform, Text } from 'react-native';
 import { AnimatedRegion } from 'react-native-maps'
 import haversine from 'haversine';
+// import getDirections from 'react-native-google-maps-directions'
 
 // For test
-const LATITUDE = 52.4339;
-const LONGITUDE = 31.0089;
+const LATITUDE = 37.3822;
+const LONGITUDE = -122.01;
 
-const LATT_DELTA = 0.009;
-const LONG_DELTA = 0.009;
+const LATT_DELTA = 0.001;
+const LONG_DELTA = 0.001;
 
 /*
     GPS Navigator container component
@@ -24,21 +25,24 @@ const testData = {
         driverId: 195,
         finish: {
           id: 871,
-          latitude: 53.9006011,
-          longitude: 27.558972,
+          latitude: 37.2297,
+          longitude: -121.786,
           name: 'Минск, Беларусь',
         },
         id: 469,
         start: {
           id: 872,
-          latitude: 52.4313388,
-          longitude: 30.99367,
+          latitude: 37.3822,
+          longitude: -122.01,
           name: 'Гомель, Беларусь',
         },
         vehicleId: 102,
     },
     waybillId: 469,
 }
+
+function decode(t,e){for(var n,o,u=0,l=0,r=0,d= [],h=0,i=0,a=null,c=Math.pow(10,e||5);u<t.length;){a=null,h=0,i=0;do a=t.charCodeAt(u++)-63,i|=(31&a)<<h,h+=5;while(a>=32);n=1&i?~(i>>1):i>>1,h=i=0;do a=t.charCodeAt(u++)-63,i|=(31&a)<<h,h+=5;while(a>=32);o=1&i?~(i>>1):i>>1,l+=n,r+=o,d.push([l/c,r/c])}return d=d.map(function(t){return{latitude:t[0],longitude:t[1]}})}
+
 
 class MapContainer extends PureComponent {
 
@@ -49,7 +53,7 @@ class MapContainer extends PureComponent {
             gpsData: {},
             latitude: LATITUDE,
             longitude: LONGITUDE,
-            driverRouteCoords: testData, 
+            driverRouteCoordsData: testData, 
             routeCoordinates: [],
             distanceTravelled: 0,
             prevLatLng: {},
@@ -61,7 +65,7 @@ class MapContainer extends PureComponent {
             }),
         };
     }
-
+    
     calcDistance = newLatLng => {
         const { prevLatLng } = this.state;
         return haversine(prevLatLng, newLatLng) || 0;
@@ -78,7 +82,7 @@ class MapContainer extends PureComponent {
         } else {
             coordinate.timing(newCoordinate).start();
         }
-
+        
         this.setState({
             gpsData: data,
             latitude,
@@ -101,13 +105,46 @@ class MapContainer extends PureComponent {
       });
 
     componentDidMount() {
+        const { start, finish } = this.state.driverRouteCoordsData.waybill
         const options = { 
             enableHighAccuracy: true, 
             timeout: 20000, 
             maximumAge: 1000,
             distanceFilter: 10
         };
+        
         this.watchId = navigator.geolocation.watchPosition(this.geoResolve, this.geoReject, options);
+
+        const mode = 'driving'; // 'walking';
+        const origin = '37.3822,-122.01';
+        const destination = '37.2297,-121.786';
+        const APIKEY = 'AIzaSyCCqDJQC4lVsw4pDBHE9D7NbPnlLtqO4yE';
+        // const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${APIKEY}&mode=${mode}`;
+        const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${APIKEY}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => {
+                console.log(responseJson);
+                
+                if (responseJson.routes.length) {
+                    this.setState({
+                        coords: decode(responseJson.routes[0].overview_polyline.points)
+                    });
+                }
+            }).catch(e => {console.warn(e)});
+        // this.setState({
+        //     driverRouteCoordsData: [
+        //         {
+        //             "latitude": start.latitude,
+        //             "longitude": start.longitude,
+        //         },
+        //         {
+        //             "latitude": finish.latitude,
+        //             "longitude": finish.longitude,
+        //         }
+        //     ]
+        // })
     };
 
     componentWillUnmount() {
@@ -119,14 +156,17 @@ class MapContainer extends PureComponent {
     }
     
     render() {
+        const speed = this.state.gpsData?.coords && Math.round(this.state.gpsData.coords.speed)
         const mapProps = {
-            speed: this.state.gpsData.state, 
+            speed: speed || 0, 
             latitude: this.state.latitude, 
             longitude: this.state.longitude, 
             routeCoordinates: this.state.routeCoordinates,
             distanceTravelled: this.state.distanceTravelled,
             coordinate: this.state.coordinate,
+            driverRouteCoordsData: this.state.driverRouteCoordsData,
             changeMarker: this.changeMarker,
+            coords: this.state.coords
         }
 
         if(this.state.gpsData?.coords) {
